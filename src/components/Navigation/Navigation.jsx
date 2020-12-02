d1da4ae26a3c91f0c0c6fa7f268a4e8117d88c74
TDWalsh <thomas.walsh736@gmail.com>
2020-12-02 15:06:41 -0500

    right aligned the navbar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,9 +4,9 @@ import {Navbar, Nav}  from "react-bootstrap"
 const Navigation = () => {
     return (
         <>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
+        <Navbar bg="dark" variant="dark" >
+          
+          <Nav className="ml-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/about">About</Nav.Link>
             <Nav.Link href="/portfolio">Portfolio</Nav.Link>
